4eb544ad556f66ddb0a0eaa34e3c5838b8621e90
Mathis-Levieux <mathis.levieux1@gmail.com>
2024-10-13 13:15:56 +0200

    add comment

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# On vérifie si le dossier /var/lib/mysql/mysql existe, si non on initialise la base de données
+# mysql_install_db : crée les fichiers système nécessaires pour le fonctionnement de MariaDB
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
+# mysqld_safe :  script pour démarrer MariaDB avec mesures de sécurité (ex en cas de crash)
+# --skip-networking : limite aux connexions locales pour configurer sans que des connexions externes soient possibles
+# & : permet de lancer le processus en arrière-plan
 mysqld_safe --skip-networking &
 until mysqladmin ping --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 1
 done
 
+# Crée le fichier pour initialiser la base de données
 cat << EOF > init.sql
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -18,6 +24,7 @@ GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
+# Exécute le fichier avec utilisateur root
 mysql -u root -p${DB_ROOT_PASSWORD} < init.sql
 rm -f init.sql
 
